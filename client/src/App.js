9600e5e3534dd059fffd0b481942ca307548e231
Hafiz <hafizalimi77@gmail.com>
2022-09-14 19:22:45 +0100

    can now see input from the front end in the db *used axios to connect front end to backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,40 @@
 import './App.css';
+import React, {useState, useEffect} from "react"
+import Axios from "axios"
 
 function App() {
+
+  const [movieName, setMovieName] = useState('')
+  const [review, setReview] = useState('')
+
+function submitReview(){  
+  Axios.post('http://localhost:3001/api/insert', {
+    movieName: movieName, 
+    movieReview: review
+  }).then(() => {
+    alert('successful insert')
+  })
+}
+
   return (
     <div className="App">
       <div className="form">
         <h1>Crud Application</h1> 
         <label>Movie Name</label>
-        <input type="text" name="movieName"/>
+        <input 
+          type="text" 
+          name="movieName" 
+          onChange={(e) => {
+            setMovieName(e.target.value);
+          }}/>
         <label>Review</label>
-        <input type="text" name="review"/>
-        <button>Submit</button>
+        <input 
+          type="text" 
+          name="review" 
+          onChange={(e) => {
+            setReview(e.target.value)
+          }}/>
+        <button onClick={submitReview}>Submit</button>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,27 +1,32 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express();
 const mysql = require('mysql');
 
-var con = mysql.createConnection({
+var db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "password",
   database: "CRUDDatabase",
 });
 
-app.get("/", (req,res) => {
-  console.log("Connected !")
-  res.send("Connected !")
-  const sql = "INSERT INTO movie_review (movieName, movieReview) VALUES ('inception', 'very good');"
-  // const sql = "DELETE FROM movie_review WHERE movieReview = 'very good'";
-  con.query(sql, (err, result) => {
-    if (err) throw err;
-    console.log("1 record inserted");
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post("/api/insert", (req,res) => {
+  const movieName = req.body.movieName
+  const movieReview = req.body.movieReview
+
+
+  const sqlInsert = 
+    "INSERT INTO movie_review (movieName, movieReview) VALUES (?,?)"
+  db.query(sqlInsert, [movieName, movieReview], (err,result) => {
     console.log(result)
-    // console.log("Number of records deleted: " + result.affectedRows);
-  })
-  
-})
+  });
+});
+
 
 app.listen(3001, () => {
   console.log('running on port 3001')
